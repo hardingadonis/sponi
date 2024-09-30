7528d1f91c284cf7811ef593574708ca6df5cdf6
Minh Vương <hardingadonis@gmail.com>
2024-09-30 08:56:24 +0700

    add: implement i18n for english & vietnamese

---
+++ src/locales/translations/en.ts
@@ -0,0 +1,8 @@
+const English = {
+	home: 'Home',
+	accounts: 'Accounts',
+	transactions: 'Transactions',
+	overview: 'Overview',
+};
+
+export default English;
---
+++ src/locales/translations/vi.ts
@@ -0,0 +1,8 @@
+const Vietnamese = {
+	home: 'Trang chủ',
+	accounts: 'Tài khoản',
+	transactions: 'Giao dịch',
+	overview: 'Tổng quan',
+};
+
+export default Vietnamese;
