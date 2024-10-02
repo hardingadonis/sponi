6bf1cf3e306d35bb0f794e2d4f86b1e62d281f92
Minh Vương <hardingadonis@gmail.com>
2024-10-02 10:17:52 +0700

    add: use `nativewind` for css

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,36 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
 
 const Page = () => {
 	return (
-		<View style={styles.container}>
-			<View style={styles.main}>
-				<Text style={styles.title}>Hello World</Text>
-				<Text style={styles.subtitle}>This is the first page of your app.</Text>
-			</View>
+		<View className="flex-1 items-center justify-center bg-white">
+			<Text>Open up App.js to start working on your app!</Text>
+			<StatusBar style="auto" />
 		</View>
 	);
 };
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignItems: 'center',
-		padding: 24,
-	},
-	main: {
-		flex: 1,
-		justifyContent: 'center',
-		maxWidth: 960,
-		marginHorizontal: 'auto',
-	},
-	title: {
-		fontSize: 64,
-		fontWeight: 'bold',
-	},
-	subtitle: {
-		fontSize: 36,
-		color: '#38434D',
-	},
-});
 
 export default Page;
